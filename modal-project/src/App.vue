50f68ec89a1275f93725639e37cb5a2663b7806f
Gapsa922 <goncalo_dsilva@hotmail.com>
2023-03-02 16:00:26 +0000

    finish lesson 4

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+<!--   <input type="text" ref="name">
+  <button @click="handleClick">Click me</button> -->
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ Modal },
+  data(){
+    return{
+      title:'My First Vue app'
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      //is going to add a class in input as active
+      this.$refs.name.focus()
+      //is going to focus the input field
+    }
   }
+
 }
 </script>
 
@@ -22,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
